68589c81da2838095a6f3940a76bae530910c9af
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-06-22 19:50:49 -0600

    d

--- phpscripts/getAttAdjustment.php
+++ phpscripts/getAttAdjustment.php
@@ -6,30 +6,30 @@
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $id = ($request->justify);
-    $res = [];
+    $resp = [];
 
-    $sql = "SELECT * FROM attendence_adjustemnt LEFT JOIN `attendence_justifications` ON attendence_justifications.idattendence_justifications = attendence_adjustemnt.id_justification LEFT JOIN `hr_processes` ON hr_processes.idhr_processes = attendence_justifications.id_process WHERE attendence_justifications.idattendence_justifications = 1";
-
-    if($result = mysqli_query($con, $sql)){
+    $sql = "SELECT * FROM attendence_adjustemnt LEFT JOIN `attendence_justifications` ON attendence_justifications.idattendence_justifications = attendence_adjustemnt.id_justification LEFT JOIN `hr_processes` ON hr_processes.idhr_processes = attendence_justifications.id_process WHERE attendence_justifications.idattendence_justifications = $id";
+    echo($sql);
+    if($result = mysqli_query($con,$sql)){
         while($res = mysqli_fetch_assoc($result)){
-            $res['idattendence_adjustemnt'] = $result['idattendence_adjustemnt'];
-            $res['id_attendence'] = $result['id_attendence'];
-            $res['id_justification'] = $result['id_justification'];
-            $res['time_before'] = $result['time_before'];
-            $res['time_after'] = $result['time_after'];
-            $res['amount'] = $result['amount'];
-            $res['state'] = $result['state'];
-            $res['id_process'] = $result['id_process'];
-            $res['reason'] = $result['reason'];
-            $res['id_user'] = $result['id_user'];
-            $res['id_employee'] = $result['id_employee'];
-            $res['id_type'] = $result['id_type'];
-            $res['id_department'] = $result['id_department'];
-            $res['date'] = $result['date'];
-            $res['notes'] = $result['notes'];
-            $res['status'] = $result['status'];
+            $resp['idattendence_adjustemnt'] = $res['idattendence_adjustemnt'];
+            $resp['id_attendence'] = $res['id_attendence'];
+            $resp['id_justification'] = $res['id_justification'];
+            $resp['time_before'] = $res['time_before'];
+            $resp['time_after'] = $res['time_after'];
+            $resp['amount'] = $res['amount'];
+            $resp['state'] = $res['state'];
+            $resp['id_process'] = $res['id_process'];
+            $resp['reason'] = $res['reason'];
+            $resp['id_user'] = $res['id_user'];
+            $resp['id_employee'] = $res['id_employee'];
+            $resp['id_type'] = $res['id_type'];
+            $resp['id_department'] = $res['id_department'];
+            $resp['date'] = $res['date'];
+            $resp['notes'] = $res['notes'];
+            $resp['status'] = $res['status'];
         }
-        echo(json_encode($res));
+        echo(json_encode($resp));
     }else{
         http_response_code(404);
     }
