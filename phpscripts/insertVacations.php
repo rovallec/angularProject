eb6fb10755100da06db6d75e0e6a7f3520950ffb
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-06-23 11:46:52 -0600

    d

--- phpscripts/insertVacations.php
+++ phpscripts/insertVacations.php
@@ -17,15 +17,22 @@ $action = ($request->action);
 $count = ($request->count);
 $took_date = ($request->took_date);
 
-$sql = "INSERT INTO `hr_processes` (`idhr_processes`, `id_user`, `id_employee`, `id_type`, `id_department`, `date`, `notes`, `status`) VALUES (null, '$id_user', '$id_employee', '$id_type', '$id_department', '$date', '$notes', '$status');";
+$sql1 = "SELECT `idemployees` FROM `employees` LEFT JOIN `hires` ON `hires`.`idhires` = `employees`.`id_hire` LEFT JOIN `profiles` ON `profiles`.`idprofiles` = `hires`.`id_profile` WHERE `idprofiles` = $id_employee";
 
-if(mysqli_query($con, $sql)){
-    $idhr_process = mysqli_insert_id($con);
-    $sql2 = "INSERT INTO `vacations` (`idvacations`, `id_process`, `action`, `count`, `date`) VALUES (null, '$idhr_process', '$action', '$count', '$took_date');";
-    if(mysqli_query($con, $sql2)){
-        http_response_code(200);
-    }else{
-        http_response_code(404);
+if($r = mysqli_query($con, $sql1)){
+    $re = mysqli_fetch_assoc($r);
+    $id_employee = $re[0]['idemployees'];
+
+    $sql = "INSERT INTO `hr_processes` (`idhr_processes`, `id_user`, `id_employee`, `id_type`, `id_department`, `date`, `notes`, `status`) VALUES (null, '$id_user', '$id_employee', '$id_type', '$id_department', '$date', '$notes', '$status');";
+
+    if(mysqli_query($con, $sql)){
+        $idhr_process = mysqli_insert_id($con);
+        $sql2 = "INSERT INTO `vacations` (`idvacations`, `id_process`, `action`, `count`, `date`) VALUES (null, '$idhr_process', '$action', '$count', '$took_date');";
+        if(mysqli_query($con, $sql2)){
+            http_response_code(200);
+        }else{
+            http_response_code(404);
+        }
     }
 }
 ?>
