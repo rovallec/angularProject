bba8677d3db3200159f5e7b52ae5b5c07c6fe543
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-06-23 08:53:36 -0600

    a

---
+++ phpscripts/getLeaves.php
@@ -0,0 +1,32 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    require 'database.php';
+
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+
+    $id = ($request->id);
+    $return = [];
+    $i = 0;
+
+    $sql = "SELECT `hr_processes`.*, `hr_processes`.*, `users`.*, `accounts`.`name` as `departmet`, `leaves`.* , `process_types`.`idprocess_types`, `process_types`.`name` as `type` FROM `leaves` LEFT JOIN `hr_processes` ON `hr_processes`.`idhr_processes` = `leaves`.`id_process` LEFT JOIN `process_types` ON `process_types`.`idprocess_types` = `hr_processes`.`id_type` LEFT JOIN `users` ON `users`.`idUser` = `hr_processes`.`id_user` LEFT JOIN `accounts` ON `accounts`.`idaccounts` = `hr_processes`.`id_department` WHERE `id_employee` = $id;";
+
+    if($result = mysqli($con, $sql)){
+        while($res = mysqli_fetch_assoc($result)){
+            $return[$i]['id_user'] = $res['id_user'];
+            $return[$i]['id_employee'] = $id;
+            $return[$i]['id_type'] = $res['type'];
+            $return[$i]['id_department'] = $res['department'];
+            $return[$i]['date'] = $res['date'];
+            $return[$i]['notes'] = $res['notes'];
+            $return[$i]['status'] = $res['status'];
+            $return[$i]['motive'] = $res['motive'];
+            $return[$i]['approved_by'] = $res['approved_by'];
+            $return[$i]['start'] = $res['start'];
+            $return[$i]['end'] = $res['end'];
+            $i++;
+        }
+        echo(json_encode($return));
+    }
+?>
