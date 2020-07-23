f46ef588fa98febc8bddd09d9f90596288c94b2f
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-07-23 17:09:32 -0600

    a

--- phpscripts/insertInsurance.php
+++ phpscripts/insertInsurance.php
@@ -26,9 +26,9 @@ $in_status = ($request->in_status);
 
 $sql = "INSERT INTO `minearsol`.`hr_processes` (`idhr_processes`, `id_user`, `id_employee`, `id_type`, `id_department`, `date`, `notes`, `status`) VALUES (NULL, '$id_user', '$id_employee', '$id_type', '$id_department', '$date', '$notes', '$status');";
 if(mysqli_query($con,$sql)){
-    $id_processes = mysqli_inset_id($con);
+    $id_processes = mysqli_insert_id($con);
 }
-$sql2 = "INSERT INTO `minearsol`.`insurances` (`idinsurances`, `id_process`, `plan`, `license`, `cert`, `contractor`, `place`, `reception`, `delivered`, `status`) VALUES (NULL, '$id_processes', '$plan', '$license', '$cert', '$contractor', '$place', '$reception', '$delivered, '$status');";
+$sql2 = "INSERT INTO `minearsol`.`insurances` (`idinsurances`, `id_process`, `plan`, `license`, `cert`, `contractor`, `place`, `reception`, `delivered`, `status`) VALUES (NULL, '$id_processes', '$plan', NULL, '$cert', '$contractor', '$place', NULL, NULL, '$status');";
 if(mysqli_query($con,$sql2)){
     http_response_code(200);
 }else{
