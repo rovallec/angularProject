304e95796ad5727d2531c086591fc4e425639b1c
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-07-27 18:33:06 -0600

    a

--- phpscripts/updateInsurances.php
+++ phpscripts/updateInsurances.php
@@ -6,13 +6,6 @@ require 'database.php';
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
-$id_user = ($request->id_user);
-$id_employee = ($request->id_employee);
-$id_type = ($request->id_type);
-$id_department = ($request->id_department);
-$date = ($request->date);
-$notes = ($request->notes);
-$status = ($request->status);
 $idinsurances = ($request->idinsurances);
 $id_process = "";
 $plan = ($request->plan);
@@ -24,7 +17,7 @@ $reception = "";
 $delivered = "";
 $in_status = ($request->in_status);
 
-$sql = "UPDATE `insurances` SET `plan` = '$plan', `license` = '$license', `cert` = '$cert', `status` = '$in_status' WHERE `idinsurances` = '$idinsurances';"
+$sql = "UPDATE `insurances` SET `plan` = '$plan', `license` = '$license', `cert` = '$cert', `status` = '$in_status' WHERE `idinsurances` = '$idinsurances';";
 
 if(mysqli_query($con,$sql)){
     http_response_code(200);
