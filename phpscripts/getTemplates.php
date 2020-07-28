860f1804edd7019a924cfb7dce6812a50026fb4d
Raul Ovalle <raul.ovalle@nearsol.gt>
2020-07-28 14:38:48 -0600

    a

--- phpscripts/getTemplates.php
+++ phpscripts/getTemplates.php
@@ -3,12 +3,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 require 'database.php';
 
-$postdata = file_get_contents("php://input");
-$request = json_decode($postdata);
-
-$id_role = ($request->id_role);
-$id_profile = ($request->id_profile);
-
 $proccesses = [];
 $i = 0;
 
@@ -18,7 +12,7 @@ $sql = "select * from process_types WHERE `idprocess_types` > 7;";
         while($row = mysqli_fetch_assoc($result)){
             $proccesses[$i]['idprocesses'] = $row['idprocesses'];
             $proccesses[$i]['name'] = $row['name'];
-            $proccesses[$i]['descritpion'] = $row['description'];Aa
+            $proccesses[$i]['descritpion'] = $row['description'];
             $i++;
         };
         echo json_encode($proccesses);
