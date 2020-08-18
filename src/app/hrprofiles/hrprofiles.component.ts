445ba5b1e8a38429de1c7b83014e8c25b9d55db3
rovallec <raul.ovalle@nearsol.gt>
2020-08-18 13:28:05 -0600

    a

--- src/app/hrprofiles/hrprofiles.component.ts
+++ src/app/hrprofiles/hrprofiles.component.ts
@@ -32,6 +32,7 @@ export class HrprofilesComponent implements OnInit {
   checkDate1: string;
   checkDate2: string;
   checkDay: string;
+  vacationsEarned: number;
 
   beneficiaryName: string;
   todayDate: string = new Date().getFullYear().toString() + "-" + (new Date().getMonth() + 1).toString().padStart(2, "0") + "-" + (new Date().getDate()).toString().padStart(2, "0");
@@ -226,8 +227,11 @@ export class HrprofilesComponent implements OnInit {
     });
 
     this.apiService.getEmployeeId({ id: this.route.snapshot.paramMap.get('id') }).subscribe((emp: employees) => {
+      this.profile[0].date_joining = emp.hiring_date;
       this.activeEmp = emp.idemployees;
     })
+
+    this.earnVacations = (new Date(this.todayDate).getMonth() - new Date(this.profile[0].date_joining).getMonth() + ((new Date(this.todayDate).getFullYear() - new Date(this.profile[0].date_joining).getFullYear())*12))*1.25;
 
     this.getAttendences(this.todayDate);
     this.attAdjudjment.id_user = this.authUser.getAuthusr().iduser;
@@ -306,6 +310,7 @@ export class HrprofilesComponent implements OnInit {
   insertAdjustment() {
     this.apiService.insertAttJustification(this.attAdjudjment).subscribe((str: string) => {
       this.getAttAdjustemt();
+      this.cancelView();
     });
   }
 
@@ -322,17 +327,17 @@ export class HrprofilesComponent implements OnInit {
   }
 
   getVacations() {
-    this.earnVacations = 0;
+    this.earnVacations = this.earnVacations;
     this.tookVacations = 0;
     this.availableVacations = 0;
     this.apiService.getVacations({ id: this.route.snapshot.paramMap.get('id') }).subscribe((res: vacations[]) => {
       this.showVacations = res;
       res.forEach(vac => {
         if (vac.action == "Add") {
-          this.earnVacations++;
+          this.earnVacations = this.earnVacations + parseFloat(vac.count);
         }
         if (vac.action == "Take") {
-          this.tookVacations++;
+          this.tookVacations = this.tookVacations + parseFloat(vac.count);
         }
       })
       this.availableVacations = this.earnVacations - this.tookVacations;
