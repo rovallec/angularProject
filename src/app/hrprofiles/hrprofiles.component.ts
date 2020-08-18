134a31c4963ece658750a3162acd6ca88f01e2ae
rovallec <raul.ovalle@nearsol.gt>
2020-08-18 14:05:29 -0600

    a

--- src/app/hrprofiles/hrprofiles.component.ts
+++ src/app/hrprofiles/hrprofiles.component.ts
@@ -229,6 +229,7 @@ export class HrprofilesComponent implements OnInit {
     this.apiService.getEmployeeId({ id: this.route.snapshot.paramMap.get('id') }).subscribe((emp: employees) => {
       this.profile[0].date_joining = emp.hiring_date;
       this.activeEmp = emp.idemployees;
+      console.log(new Date(this.todayDate).getMonth().toString() + " " + new Date(this.profile[0].date_joining).getMonth().toString())
       this.vacationsEarned = ((new Date(this.todayDate).getMonth() - new Date(this.profile[0].date_joining).getMonth()) + ((new Date(this.todayDate).getFullYear() - new Date(this.profile[0].date_joining).getFullYear())*12))*1.25;
     })
 
