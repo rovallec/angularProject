6ab3db2ced61a2de5a99adb1d9c4ea05e6041de4
rovallec <raul.ovalle@nearsol.gt>
2020-08-31 09:37:50 -0600

    l

--- phpscripts/letterBaja.php
+++ phpscripts/letterBaja.php
@@ -98,7 +98,7 @@ echo "
 <td colspan='4' style='height:50px'></td>
 </tr>
 <tr>
-<td coslpan='4' style='text-aligns:center'><img style='width:100px;height:50px' src='http://200.94.251.67/uploads/'" . $iduser . "_Signatures_.jpg</td>
+<td coslpan='4' style='text-aligns:center'><img style='width:100px;height:50px' src='http://200.94.251.67/uploads/" . $iduser . "_Signatures_.jpg'</td>
 </tr>
 <tr>
 <td colspan='4' style=' text-align: center'>" . $user . "
