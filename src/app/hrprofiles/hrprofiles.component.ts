dfbcb5cfc4d5a6c25ae6062ee7c16666cdfcc43e
rovallec <raul.ovalle@nearsol.gt>
2020-08-31 18:26:29 -0600

    0

--- src/app/hrprofiles/hrprofiles.component.ts
+++ src/app/hrprofiles/hrprofiles.component.ts
@@ -605,33 +605,26 @@ export class HrprofilesComponent implements OnInit {
     if (this.newAudience === "YES" && this.newSuspension === "NO") {
       this.apiService.insertDPA(this.activeRequest).subscribe((str: string) => {
         this.getDisciplinaryProcesses();
-        this.newRequest = false;
-        this.storedRequest = false;
       })
     }
     if (this.newSuspension === "YES" && this.newAudience === "NO") {
       this.apiService.insertDPS(this.activeRequest).subscribe((str: string) => {
         this.getDisciplinaryProcesses();
-        this.newRequest = false;
-        this.storedRequest = false;
       })
     }
     if (this.newSuspension === "YES" && this.newAudience === "YES") {
       this.apiService.insertDPSA(this.activeRequest).subscribe((str: string) => {
         this.getDisciplinaryProcesses();
-        this.newRequest = false;
-        this.storedRequest = false;
       })
     }
     if (this.newSuspension === "NO" && this.newAudience === "NO") {
       this.apiService.insertDP(this.activeRequest).subscribe((str: string) => {
         this.getDisciplinaryProcesses();
-        this.newRequest = false;
-        this.storedRequest = false;
       })
     }
     this.newRequest = false;
-    this.editRequest = false;
+    this.storedRequest = false;
+    console.log(this.newRequest + " " + this.storedRequest)
   }
 
   pushSuspensionStart(str: any) {
