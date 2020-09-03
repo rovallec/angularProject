8fdb00a242e3f7b4e383dc5d069b97600c3ed938
rovallec <raul.ovalle@nearsol.gt>
2020-09-03 08:44:48 -0600

    a

--- phpscripts/insertAttendences.php
+++ phpscripts/insertAttendences.php
@@ -9,6 +9,7 @@
     $att = [];
     $id = "";
     $date = "";
+    $d_off = "";
 
     if(isset($postdata) && !empty($postdata)){
         $request = json_decode($postdata);
@@ -23,16 +24,25 @@
             $scheduled = ($de->scheduled);
             $worked = ($de->worked_time);
             $id = ($de->id_wave);
+            $d_off = ($de->day_off1);
             $sql = $sql . "(NULL, '$id_employee', '$date', '$scheduled', '$worked')";
         }
-        $sql = $sql . ";";
         if(mysqli_query($con, $sql)){
-            $sql2 = "SELECT * FROM (SELECT `att`.`idattendences`, `hires`.`id_wave`, `employees`.`idemployees`, `hires`.`nearsol_id`, `employees`.`client_id`, `profiles`.`first_name`, `profiles`.`second_name`, `profiles`.`first_lastname`, `profiles`.`second_lastname`, `att`.`date`, `att`.`worked_time`, `att`.`scheduled`,`schedules`.`days_off`, `profiles`.`status`
+            if($d_off == "CORRECT"){
+                $sql2 = "SELECT * FROM (SELECT `att`.`idattendences`, `hires`.`id_wave`, `employees`.`idemployees`, `hires`.`nearsol_id`, `employees`.`client_id`, `profiles`.`first_name`, `profiles`.`second_name`, `profiles`.`first_lastname`, `profiles`.`second_lastname`, `att`.`date`, `att`.`worked_time`, `att`.`scheduled`,`schedules`.`days_off`, `profiles`.`status`
+                FROM `hires`
+                    LEFT JOIN `profiles` ON `profiles`.`idprofiles` = `hires`.`id_profile`
+                    LEFT JOIN `schedules` ON `schedules`.`idschedules` = `hires`.`id_schedule`
+                    LEFT JOIN `employees` ON `employees`.`id_hire` = `hires`.`idhires`
+                    LEFT JOIN (SELECT * FROM `attendences` WHERE `date` = '$date') AS `att` ON `att`.`id_employee` = `employees`.`idemployees`) AS `attend`";
+            }else{
+                $sql2 = "SELECT * FROM (SELECT `att`.`idattendences`, `hires`.`id_wave`, `employees`.`idemployees`, `hires`.`nearsol_id`, `employees`.`client_id`, `profiles`.`first_name`, `profiles`.`second_name`, `profiles`.`first_lastname`, `profiles`.`second_lastname`, `att`.`date`, `att`.`worked_time`, `att`.`scheduled`,`schedules`.`days_off`, `profiles`.`status`
             FROM `hires`
                 LEFT JOIN `profiles` ON `profiles`.`idprofiles` = `hires`.`id_profile`
                 LEFT JOIN `schedules` ON `schedules`.`idschedules` = `hires`.`id_schedule`
                 LEFT JOIN `employees` ON `employees`.`id_hire` = `hires`.`idhires`
                 LEFT JOIN (SELECT * FROM `attendences` WHERE `date` = '$date') AS `att` ON `att`.`id_employee` = `employees`.`idemployees`) AS `attend` WHERE `id_wave` = $id";
+            }
                 if($result = mysqli_query($con, $sql2))
                 {
                     $i = 0;
