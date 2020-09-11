b0a4ca06255aa0372193af9391a858c4094f2ad1
rovallec <raul.ovalle@nearsol.gt>
2020-09-11 00:41:57 -0600

    a

--- src/app/active-analysis/active-analysis.component.ts
+++ src/app/active-analysis/active-analysis.component.ts
@@ -43,6 +43,6 @@ export class ActiveAnalysisComponent implements OnInit {
     if(!this.isExportable){
       alert("Missing information to generate the report");
     }
-      window.open("http://200.94.251.67/phpscripts/exportActiveAnalysis.php?date='" + this.dateFrom + "' AND '" + this.dateTo)
+      window.open("http://200.94.251.67/phpscripts/exportActiveAnalysis.php", "_blank")
     }
 }
