3c35bea4be4554329a8d0992aef242b815e461d8
rovallec <raul.ovalle@nearsol.gt>
2020-11-27 09:02:04 -0600

    a

--- phpscripts/getfilteredAccounts.php
+++ phpscripts/getfilteredAccounts.php
@@ -12,11 +12,12 @@ $res = [];
 $i = 0;
 
 $sql1="SELECT * FROM `accounts` WHERE idaccounts = $id";
+
 if($result2 = mysqli_query($con, $sql1)){
     while($row = mysqli_fetch_assoc($result2)){
         $id_client = $row['id_client'];
     }
-    $sql = "Select * from `accounts` WHERE id_client = $id_client;";
+    $sql = "Select * from `accounts` WHERE id_client = $id_client OR id_client = 2;";
     if($request = mysqli_query($con,$sql)){
         while($row = mysqli_fetch_assoc($request)){
             $res[$i]['idaccounts'] = $row['idaccounts'];
