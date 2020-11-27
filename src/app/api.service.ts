1ed6c4cc6ac48cd2efeebca88bb290251bf5925c
avielman <avielman@gmail.com>
2020-11-27 13:39:42 -0600

    Se corrige la ruta oficial

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -19,7 +19,8 @@ export class ApiService {
 prof:profiles[] = [];
 id_profile:number;
 
-PHP_API_SERVER = "http://localhost/angularproject";
+//PHP_API_SERVER = "http://localhost/angularproject";
+PHP_API_SERVER = "http://200.94.251.67";
 
 readProfiles():Observable<profiles[]>{
   return this.httpClient.get<profiles[]>(`${this.PHP_API_SERVER}/phpscripts/read_profiles.php`);
