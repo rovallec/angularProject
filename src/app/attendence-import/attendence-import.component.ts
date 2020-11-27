1f1d05ad3e8d8998dd1893c82d38ed4247d380f5
rovallec <raul.ovalle@nearsol.gt>
2020-11-27 11:25:01 -0600

    a

--- src/app/attendence-import/attendence-import.component.ts
+++ src/app/attendence-import/attendence-import.component.ts
@@ -169,6 +169,6 @@ export class AttendenceImportComponent implements OnInit {
 
   getRoster(){
     console.log(this.selectedAccount);
-    window.open("http://200.94.251.67/phpscripts/exportRoster.php?acc=" + this.selectedAccount.idaccounts, "_blank")
+    window.open("http://200.94.251.67/phpscripts/exportRoster.php?acc=" + this.selectedAccount, "_blank")
   }
 }
