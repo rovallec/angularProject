5f902fc3a4b66fcf1bd9b152c8aa738fa34598ff
rovallec <raul.ovalle@nearsol.gt>
2020-12-03 12:23:54 -0600

    a

--- src/app/periods/periods.component.ts
+++ src/app/periods/periods.component.ts
@@ -609,11 +609,13 @@ export class PeriodsComponent implements OnInit {
                             });
 
                             if (!activeLeav && !activeVac && !activeDp) {
-                              if (attendance.scheduled == 'OFF') {
+                              if (attendance.scheduled == 'OFF' && parseFloat(attendance.worked_time) == 0) {
                                 this.daysOff = this.daysOff + 1;
                                 attendance.balance = "OFF";
                               } else {
-                                this.roster = this.roster + parseFloat(attendance.scheduled);
+                                if(attendance.scheduled != 'OFF'){
+                                  this.roster = this.roster + parseFloat(attendance.scheduled);
+                                }
                                 if (parseFloat(attendance.worked_time) == 0) {
                                   ad.forEach(adjustment => {
                                     if (adjustment.id_attendence === attendance.idattendences) {
