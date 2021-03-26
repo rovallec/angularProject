cc88356b1eca676585c192333c2e56b83f59db7a
rovallec <raul.ovalle@nearsol.gt>
2021-03-25 22:17:53 -0600

    echo filtered employees

--- phpscripts/getFilteredEmployees.php
+++ phpscripts/getFilteredEmployees.php
@@ -42,7 +42,6 @@ if($dp != '4' && $dp != '28'){
 if($filter === 'client_id'){
     $sql = "SELECT `employees`.`base_payment`, `employees`.`productivity_payment`, `employees`.*, `hires`.`id_profile`, `hires`.`nearsol_id`, `users`.`user_name`, `accounts`.`name`, `profiles`.`first_name`, `profiles`.`second_name`, `profiles`.`first_lastname`, `profiles`.`second_lastname`, `profiles`.`dpi`  FROM `employees` LEFT JOIN `users` ON `users`.`idUser` = `employees`.`reporter` LEFT JOIN `accounts` ON `accounts`.`idaccounts` = `employees`.`id_account` LEFT JOIN `hires` ON `hires`.`idhires` = `employees`.`id_hire` LEFT JOIN `profiles` ON `profiles`.`idprofiles` = `hires`.`id_profile` WHERE (`$filter` = '$value');";
 }
-echo($sql);
 if($request = mysqli_query($con,$sql)){
     while($row = mysqli_fetch_assoc($request)){
         $res[$i]['id_profile'] = $row['id_profile'];
