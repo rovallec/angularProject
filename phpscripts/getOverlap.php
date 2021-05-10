32d9516f63349565e347cafb5992b59ad120ad15
rovallec <raul.ovalle@nearsol.gt>
2021-05-10 15:24:02 -0600

    test overlap

--- phpscripts/getOverlap.php
+++ phpscripts/getOverlap.php
@@ -10,16 +10,40 @@ $nd = ($request->end);
 $res = [];
 $i = 0;
 
-$sql = "SELECT hires.nearsol_id, accounts.name AS `acc`, profiles.idprofiles, employees.idemployees, profiles.first_name, profiles.second_name, profiles.first_lastname, profiles.second_lastname ,employees.client_id,`tmp`.id_employee, count(`tmp`.idattendences) AS `cnt`, group_concat(`tmp`.date) AS `dates` FROM 
+$sql = "SELECT hires.nearsol_id, accounts.name AS `acc`,
+profiles.idprofiles, employees.idemployees, profiles.first_name, profiles.second_name, profiles.first_lastname,
+profiles.second_lastname ,employees.client_id,`tmp`.id_employee, count(`tmp`.idattendences) AS `cnt`, group_concat(`tmp`.date) AS `dates` FROM 
 (SELECT attendences.* FROM attendences
 LEFT JOIN hr_processes ON attendences.id_employee = hr_processes.id_employee
 INNER JOIN vacations ON vacations.id_process = hr_processes.idhr_processes AND attendences.date = vacations.date AND vacations.count > 0.5
-WHERE attendences.worked_time > 0 AND attendences.date BETWEEN '$st' AND '$nd' AND hr_processes.status = 'PENDING'
+WHERE attendences.worked_time > 0 AND attendences.date BETWEEN '$start' AND '$end' AND hr_processes.status = 'PENDING'
 UNION
 SELECT attendences.* FROM attendences
 LEFT JOIN hr_processes ON attendences.id_employee = hr_processes.id_employee
 INNER JOIN leaves ON attendences.date BETWEEN leaves.start AND leaves.end AND leaves.id_process = hr_processes.idhr_processes
-WHERE attendences.worked_time > 0 AND attendences.date BETWEEN '$st' AND '$nd' AND hr_processes.status = 'PENDING') as `tmp`
+WHERE attendences.worked_time > 0 AND attendences.date BETWEEN '$start' AND '$end' AND hr_processes.status = 'PENDING'
+UNION 
+SELECT idattendences, id_employee, date, scheduled, worked_time FROM (
+SELECT attendences.idattendences, attendences.id_employee, attendences.date, 
+attendences.scheduled, attendences.worked_time, COUNT(leaves.idleaves) AS `cnt`, COUNT(vacations.idvacations) AS `vac_count`,
+`susp`.`cnt` AS `suspensions_count`
+FROM attendences
+LEFT JOIN hr_processes ON attendences.id_employee = hr_processes.id_employee AND (id_type = 5 OR id_type = 4 OR id_type = 6)
+LEFT JOIN leaves ON hr_processes.idhr_processes = leaves.id_process AND (attendences.date >= leaves.start AND attendences.date <= leaves.end)
+LEFT JOIN vacations ON hr_processes.idhr_processes = vacations.id_process AND vacations.date = attendences.date
+LEFT JOIN (
+SELECT attendences.*, COUNT(idsuspensions) AS `cnt` FROM
+attendences
+INNER JOIN hr_processes ON hr_processes.id_employee = attendences.id_employee AND hr_processes.id_type = 6
+INNER JOIN disciplinary_requests ON disciplinary_requests.id_process = hr_processes.idhr_processes
+INNER JOIN disciplinary_processes ON disciplinary_processes.id_request = disciplinary_requests.iddisciplinary_requests
+INNER JOIN suspensions ON suspensions.id_disciplinary_process = disciplinary_processes.iddisciplinary_processes 
+			AND (suspensions.day_1 = attendences.date OR suspensions.day_2 = attendences.date OR suspensions.day_3 = attendences.date OR suspensions.day_4 = attendences.date)
+WHERE hr_processes.status = 'DISPATCHED'
+GROUP BY idattendences) AS `susp` ON `susp`.idattendences = attendences.idattendences
+WHERE attendences.date BETWEEN '$start' AND '$end' AND (hr_processes.status = 'PENDING') GROUP BY attendences.idattendences) AS `leaves`
+WHERE `cnt` > 1 OR `vac_count` > 1 OR `suspensions_count` > 1 OR (`vac_count` > 0 AND `cnt` > 0) OR (`vac_count` > 0 AND `suspensions_count` > 0) OR (`suspensions_count` > 0 AND `cnt` > 0)
+) as `tmp`
 INNER JOIN employees ON employees.idemployees = `tmp`.id_employee
 INNER JOIN hires ON hires.idhires = employees.id_hire
 INNER JOIN profiles ON profiles.idprofiles = hires.id_profile
