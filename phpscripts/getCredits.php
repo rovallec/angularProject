5bff110661d13a051041ab3307c6a859ce2c66fa
rovallec <raul.ovalle@nearsol.gt>
2021-05-18 14:14:18 -0600

    asd

--- phpscripts/getCredits.php
+++ phpscripts/getCredits.php
@@ -11,7 +11,7 @@ $request = json_decode($postdata);
 $id = ($request->id);
 $period = ($request->period);
 
-$sql = "select profiles.idprofiles, credits.idcredits, profiles.first_name, profiles.second_name, profiles.first_lastname, profiles.second_lastname, credits.status, credits.type, credits.amount from credits left join payments on payments.idpayments = credits.id_payment left join employees on employees.idemployees = payments.id_employee left join periods on periods.idperiods = payments.id_period left join hires on hires.idhires = employees.id_hire left join profiles on profiles.idprofiles = hires.id_profile where id_employee = $id AND idperiods = $period;";
+$sql = "select profiles.idprofiles, credits.idcredits, profiles.first_name, profiles.second_name, profiles.first_lastname, profiles.second_lastname, credits.status, credits.type, credits.amount, payments.idpayments from credits left join payments on payments.idpayments = credits.id_payment left join employees on employees.idemployees = payments.id_employee left join periods on periods.idperiods = payments.id_period left join hires on hires.idhires = employees.id_hire left join profiles on profiles.idprofiles = hires.id_profile where id_employee = $id AND idperiods = $period;";
 
 if($request = mysqli_query($con,$sql)){
     while($row = mysqli_fetch_assoc($request)){
