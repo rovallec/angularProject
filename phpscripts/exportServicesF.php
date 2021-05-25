00ecdd0b64b0d7dd985a819264c87ce3f50b954e
rovallec <raul.ovalle@nearsol.gt>
2021-05-25 17:32:55 -0600

    services

--- phpscripts/exportServicesF.php
+++ phpscripts/exportServicesF.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename="' . "roster.csv" . '"');
+header('Content-Disposition: attachment; filename="' . "services.csv" . '"');
 require 'database.php';
 
 $roster = [];
