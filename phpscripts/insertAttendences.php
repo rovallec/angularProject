c76699662d8fcb6c405291501d7c6d7e953b6b4f
rovallec <raul.ovalle@nearsol.gt>
2021-05-28 17:03:27 -0600

    a

--- phpscripts/insertAttendences.php
+++ phpscripts/insertAttendences.php
@@ -24,7 +24,7 @@
             $id = validarDatos($de->id_wave);
             $d_off = validarDatos($de->day_off1);
             $sql = "INSERT INTO attendences (idattendences, id_employee, date, scheduled, worked_time)  
-                        SELECT * FROM (SELECT null,$id_employee AS `1`,'$date' AS `2`,'$scheduled' AS `3`,'$worked' AS `4`) 
+                        SELECT * FROM (SELECT null,$id_employee AS `1`,'$date' AS `2`,ROUND($scheduled,3) AS `3`,ROUND($worked,3) AS `4`) 
                         AS tmp WHERE NOT EXISTS (SELECT date FROM attendences WHERE id_employee = $id_employee AND date = '$date') LIMIT 1;";
             if(mysqli_query($con, $sql)){
             }else{
