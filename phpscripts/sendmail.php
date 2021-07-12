887b4d718828ae13cb166692c5291595b9c6ee75
rovallec <raul.ovalle@nearsol.gt>
2021-07-12 15:37:28 -0600

    a

---
+++ phpscripts/sendmail.php
@@ -0,0 +1,33 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "tickets@nearsol.us";
+$mail->Password   = "Nearsol.2020!";
+
+$mail->IsHTML(true);
+$mail->AddAddress("raul.ovalle@nearsol.gt", "Raul Ovalle");
+$mail->SetFrom("tickets@nearsol.us", "MiNearsol");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+?>;
