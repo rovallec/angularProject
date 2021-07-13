4ba300caafc28ed8c06eed2d95d500793f6f1fa8
rovallec <raul.ovalle@nearsol.gt>
2021-07-13 17:13:08 -0600

    a

--- phpscripts/sendmail.php
+++ phpscripts/sendmail.php
@@ -119,253 +119,255 @@ if($result = mysqli_query($con, $sql))
 		$liquido = ($row['credits'] - $row['debits']);
 	}
 
-		$mail = new PHPMailer();
-		$mail->IsSMTP();
-		$mail->Mailer = "smtp";
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
+	$mail->Mailer = "smtp";
 
-		$mail->SMTPDebug  = 0;
-		$mail->SMTPAuth   = TRUE;
-		$mail->SMTPSecure = "tls";
-		$mail->Port       = 587;
-		$mail->Host       = "smtp.gmail.com";
-		$mail->Username   = "tickets@nearsol.us";
-		$mail->Password   = "2021!N.ti@lf@-Rp0713";
+	$mail->SMTPDebug  = 0;
+	$mail->SMTPAuth   = TRUE;
+	$mail->SMTPSecure = "tls";
+	$mail->Port       = 587;
+	$mail->Host       = "smtp.gmail.com";
+	$mail->Username   = "tickets@nearsol.us";
+	$mail->Password   = "2021!N.ti@lf@-Rp0713";
 
-		$mail->IsHTML(true);
-		$mail->AddAddress($email, $employee_name);
-		$mail->SetFrom("tickets@nearsol.us", "MiNearsol Paystub");
-		$mail->Subject = "Recibo de Sueldos Del $start  Al  $end";
-		$content = "
-		<body>
-			<table style='margin-top:25px;width:90%;margin-left:5%'>
-				<tbody>
-					<tr>
-						<td>$society</td>
-						<td style='text-align:right'>NIT: $employeer_nit</td>
-					</tr>
-					<tr style='margin-left:2.5%;width:95%'>
-						<td>Jornada: $account</td>
-						<td style='text-align:right'>Nomina No.: $idpayments</td>
-					</tr>
-					<tr style='margin-left:2.5%;width:95%'>
-						<td>Recibo de Pago de Sueldos y Salarios</td>
-						<td></td>
-					</tr>
-					<tr style='border:solid black 2px; width:100%'>
-						<td colspan='2'>
-							<table style='width:100%;margin-top:2px;border:solid black 2px'>
-								<tbody>
-									<tr>
-										<td>Por el Periodo del $start</td>
-										<td>Al $end</td>
-										<td>FORMA DE PAGO</td>
-									</tr>
-									<tr>
-										<td>Codigo de Empleado $nearsol_id</td>
-										<td>NIT del empleado $nit</td>
-										<td>$type $number</td>
-									</tr>
-									<tr>
-										<td>No. Afiliacion IGSS $iggs</td>
-										<td>Supervisor: $user_name</td>
-										<td>$bank</td>
-									</tr>
-								</tbody>
-							</table>
-						</td>
-					</tr>
-					<tr>
-						<td colspan='2'>
-							<table style='width:100%;margin-top:0px;border:solid black 2px'>
+	$mail->IsHTML(true);
+	$mail->AddAddress($email, $employee_name);
+	$mail->SetFrom("tickets@nearsol.us", "MiNearsol Paystub");
+	$mail->Subject = "Recibo de Sueldos Del $start  Al  $end";
+	$content = "
+	<body>
+		<table style='margin-top:25px;width:90%;margin-left:5%'>
+			<tbody>
+				<tr>
+					<td>$society</td>
+					<td style='text-align:right'>NIT: $employeer_nit</td>
+				</tr>
+				<tr style='margin-left:2.5%;width:95%'>
+					<td>Jornada: $account</td>
+					<td style='text-align:right'>Nomina No.: $idpayments</td>
+				</tr>
+				<tr style='margin-left:2.5%;width:95%'>
+					<td>Recibo de Pago de Sueldos y Salarios</td>
+					<td></td>
+				</tr>
+				<tr style='border:solid black 2px; width:100%'>
+					<td colspan='2'>
+						<table style='width:100%;margin-top:2px;border:solid black 2px'>
+							<tbody>
 								<tr>
-									<td>Dias del Periodo:</td>
-									<td>15</td>
-									<td>Dias Descontados:</td>
-									<td>$discounted_days</td>
-									<td>Horas Extraordinarias:</td>
-									<td>$ot_hours</td>
+									<td>Por el Periodo del $start</td>
+									<td>Al $end</td>
+									<td>FORMA DE PAGO</td>
 								</tr>
 								<tr>
-									<td></td>
-									<td></td>
-									<td>Horas Descontadas:</td>
-									<td>$discounted_hours</td>
-									<td>Horas Asueto:</td>
-									<td>$holidays_hours</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<table style='width:100%;margin-top:0px;border:solid black 2px'>
-								<tr>
-									<td>Sueldo Ordinario:</td>
-									<td>$base</td>
+									<td>Codigo de Empleado $nearsol_id</td>
+									<td>NIT del empleado $nit</td>
+									<td>$type $number</td>
 								</tr>
 								<tr>
-									<td>Sueldo ExtraOrdinario:</td>
-									<td>$ot</td>
+									<td>No. Afiliacion IGSS $iggs</td>
+									<td>Supervisor: $user_name</td>
+									<td>$bank</td>
 								</tr>
-								<tr>
-									<td>Sueldo extraOrdinario Dias De Asueto:</td>
-									<td>$holidays</td>
-								</tr>
-								<tr>
-									<td>Bonificación Incentivo (Decretos 78-89,7-2000 y 37-2001):</td>
-									<td>$decreto</td>
-								</tr>
-								<tr>
-									<td>Bonificación por Productividad (Decretos 78-89,7-2000 y 37-2001):</td>
-									<td>$bonificaciones</td>
-								</tr>
-								<tr>
-									<td>Bonificación por Eficiencia (Decretos 78-89,7-2000 y 37-2001) Bonos:</td>
-									<td>$eficiencia</td>
-								</tr>
-								<tr>
-									<td>Bonificación por Eficiencia (Decretos 78-89,7-2000 y 37-2001) Ajustes:</td>
-									<td>$ajustes</td>
-								</tr>
-								<tr>
-									<td style='color:white'>.</td>
-									<td></td>
-								</tr>
-							</table>
-						</td>
-						<td>
-							<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
-								<tr>
-									<td>IGSS Laboral:</td>
-									<td>($igss)</td>
-								</tr>
-								<tr>
-									<td>Descuentso (Bus/Parqueo):</td>
-									<td>($parqueo)</td>
-								</tr>
-								<tr>
-									<td>Otras Deducciones:</td>
-									<td>($otras_deducciones)</td>
-								</tr>
-								<tr>
-									<td>Anticipo Sobre Sueldos:</td>
-									<td>($anticipos)</td>
-								</tr>
-								<tr>
-									<td>ISR:</td>
-									<td>($isr)</td>
-								</tr>
-								<tr>
-									<td style='color:white'>.</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td style='color:white'>.</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td style='color:white'>.</td>
-									<td></td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-					<tr>
-						<td colspan='2'>
-							<div style='position:relative;width:100%'>
-								<div style='width:33%;float:left'>
-									<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
-										<tbody>
-											<tr>
-												<td>Total Devengado:</td>
-												<td>$total_cred</td>
-											</tr>
-											<tr>
-												<td style='border-bottom:solid black 3px'>Total Descuentos:</td>
-												<td style='border-bottom:solid black 3px'>($total_deb)</td>
-											</tr>
-											<tr>
-												<td>Sueldo Liquido:</td>
-												<td>$liquido</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-								<div style='width:66.6%;float:left;margin-left:0.4%'>
-									<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
-										<tbody>
-											<tr>
-												<td style='color:white'>f</td>
-												<td></td>
-												<td></td>
-												<td></td>
-											</tr>
-											<tr>
-												<td style='text-align:right;border-bottom:solid white 1px'>f:</td>
-												<td></td>
-												<td></td>
-												<td></td>
-											</tr>
-											<tr>
-												<td></td>
-												<td colspan='2' style='border-top:solid black 2px;text-align:center'>$employee_name</td>
-												<td></td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td colspan='2'>
-							<div style='position:relative;'>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td colspan='2'>
+						<table style='width:100%;margin-top:0px;border:solid black 2px'>
+							<tr>
+								<td>Dias del Periodo:</td>
+								<td>15</td>
+								<td>Dias Descontados:</td>
+								<td>$discounted_days</td>
+								<td>Horas Extraordinarias:</td>
+								<td>$ot_hours</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>Horas Descontadas:</td>
+								<td>$discounted_hours</td>
+								<td>Horas Asueto:</td>
+								<td>$holidays_hours</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table style='width:100%;margin-top:0px;border:solid black 2px'>
+							<tr>
+								<td>Sueldo Ordinario:</td>
+								<td>$base</td>
+							</tr>
+							<tr>
+								<td>Sueldo ExtraOrdinario:</td>
+								<td>$ot</td>
+							</tr>
+							<tr>
+								<td>Sueldo extraOrdinario Dias De Asueto:</td>
+								<td>$holidays</td>
+							</tr>
+							<tr>
+								<td>Bonificación Incentivo (Decretos 78-89,7-2000 y 37-2001):</td>
+								<td>$decreto</td>
+							</tr>
+							<tr>
+								<td>Bonificación por Productividad (Decretos 78-89,7-2000 y 37-2001):</td>
+								<td>$bonificaciones</td>
+							</tr>
+							<tr>
+								<td>Bonificación por Eficiencia (Decretos 78-89,7-2000 y 37-2001) Bonos:</td>
+								<td>$eficiencia</td>
+							</tr>
+							<tr>
+								<td>Bonificación por Eficiencia (Decretos 78-89,7-2000 y 37-2001) Ajustes:</td>
+								<td>$ajustes</td>
+							</tr>
+							<tr>
+								<td style='color:white'>.</td>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+					<td>
+						<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
+							<tr>
+								<td>IGSS Laboral:</td>
+								<td>($igss)</td>
+							</tr>
+							<tr>
+								<td>Descuentso (Bus/Parqueo):</td>
+								<td>($parqueo)</td>
+							</tr>
+							<tr>
+								<td>Otras Deducciones:</td>
+								<td>($otras_deducciones)</td>
+							</tr>
+							<tr>
+								<td>Anticipo Sobre Sueldos:</td>
+								<td>($anticipos)</td>
+							</tr>
+							<tr>
+								<td>ISR:</td>
+								<td>($isr)</td>
+							</tr>
+							<tr>
+								<td style='color:white'>.</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td style='color:white'>.</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td style='color:white'>.</td>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td colspan='2'>
+						<div style='position:relative;width:100%'>
+							<div style='width:33%;float:left'>
 								<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
 									<tbody>
 										<tr>
-											<td>Observaciones:</td>
-											<td colspan='4'></td>
+											<td>Total Devengado:</td>
+											<td>$total_cred</td>
 										</tr>
 										<tr>
-											<td style='color:white'>.</td>
-											<td colspan='4'></td>
+											<td style='border-bottom:solid black 3px'>Total Descuentos:</td>
+											<td style='border-bottom:solid black 3px'>($total_deb)</td>
+										</tr>
+										<tr>
+											<td>Sueldo Liquido:</td>
+											<td>$liquido</td>
 										</tr>
 									</tbody>
 								</table>
 							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</body>
-		";
+							<div style='width:66.6%;float:left;margin-left:0.4%'>
+								<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
+									<tbody>
+										<tr>
+											<td style='color:white'>f</td>
+											<td></td>
+											<td></td>
+											<td></td>
+										</tr>
+										<tr>
+											<td style='text-align:right;border-bottom:solid white 1px'>f:</td>
+											<td></td>
+											<td></td>
+											<td></td>
+										</tr>
+										<tr>
+											<td></td>
+											<td colspan='2' style='border-top:solid black 2px;text-align:center'>$employee_name</td>
+											<td></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td colspan='2'>
+						<div style='position:relative;'>
+							<table style='width:100%;margin-top:0px;border:solid black 2px;height:100%'>
+								<tbody>
+									<tr>
+										<td>Observaciones:</td>
+										<td colspan='4'></td>
+									</tr>
+									<tr>
+										<td style='color:white'>.</td>
+										<td colspan='4'></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+	";
 
-		$mail->MsgHTML($content); 
-		if(!$mail->Send()) {
-		$res = $mail->ErrorInfo;
-		var_dump($mail);
-		} else {
-		$res = "Successfuly Sent " . date("Y-m-d H:i:s");
-		}
+	$mail->MsgHTML($content); 
+	if(!$mail->Send()) {
+	$res = $mail->ErrorInfo;
+	var_dump($mail);
+	} else {
+	$res = "Successfuly Sent " . date("Y-m-d H:i:s");
+	}
 
-		$sql2 = "INSERT INTO paystub_details (`idpaystub_deatils`, `id_payment`, `recipent`, `sender`, `subject`, `content`, `result`)
-				VALUES (NULL, $idpayments, $email, 'tickets@nearsol.us', 'Recibo de Sueldos Del  $start Al  $end', '$content', '$res')";
+	$sql2 = "INSERT INTO paystub_details (`idpaystub_deatils`, `id_payment`, `recipent`, `sender`, `subject`, `content`, `result`)
+			VALUES (NULL, $idpayments, $email, 'tickets@nearsol.us', 'Recibo de Sueldos Del  $start Al  $end', '$content', '$res')";
 
-		if(mysqli_query($con, $sql2)){
-			$id = mysqli_insert_id($con);
-			$sql3 = "SELECT * FROM paystub_details WHERE idpaystub_details = $id";
-			if($result2 = mysqli_query($con, $sql3)){
-				while($row2 = mysqli_fetch_assoc($result2)){
-					$return['idpaystub_deatils'] = $row2['idpaystub_deatils'];
-					$return['id_payment'] = $row2['id_payment'];
-					$return['recipent'] = $row2['recipent'];
-					$return['sender'] = $row2['sender'];
-					$return['subject'] = $row2['subject'];
-					$return['content'] = $row2['content'];
-					$return['result'] = $row2['result'];
-				}
+	if(mysqli_query($con, $sql2)){
+		$id = mysqli_insert_id($con);
+		$sql3 = "SELECT * FROM paystub_details WHERE idpaystub_details = $id";
+		if($result2 = mysqli_query($con, $sql3)){
+			while($row2 = mysqli_fetch_assoc($result2)){
+				$return['idpaystub_deatils'] = $row2['idpaystub_deatils'];
+				$return['id_payment'] = $row2['id_payment'];
+				$return['recipent'] = $row2['recipent'];
+				$return['sender'] = $row2['sender'];
+				$return['subject'] = $row2['subject'];
+				$return['content'] = $row2['content'];
+				$return['result'] = $row2['result'];
 			}
 		}
+	}else{
+		echo($sql2);
+	}
 }
 echo(json_encode($return));
 ?>;
