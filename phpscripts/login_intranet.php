b44762c346c32c4ee3aa566e41e9c54e5219ee94
rovallec <raul.ovalle@nearsol.gt>
2021-07-17 13:59:38 -0600

    request

--- phpscripts/login_intranet.php
+++ phpscripts/login_intranet.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 
 $postdata = file_get_contents("php://input");
-
+$request = json_decode($postdata);
 $usr = ($request->username);
 $pss = ($request->password);
 
