5ff1d229c74fe0012c922b60f4b104a6a38c045d
rovallec <raul.ovalle@nearsol.gt>
2021-07-19 11:06:08 -0600

    asdf

--- phpscripts/change_password.php
+++ phpscripts/change_password.php
@@ -11,30 +11,27 @@ header('Access-Control-Allow-Headers: *');
 		$password = ($request->password);
 		
 		//$sql = "ALTER USER '$user'@’localhost’ IDENTIFIED BY ‘$password’;";
-		$sql1 = 'USE mysql;';
-        $sql = "UPDATE user SET authentication_string = PASSWORD('N$@dmin.2002!') WHERE user = 'rovalle' AND host='%'";
+		$sql1 = 'USE mysql'
+        $sql = "ALTER user 'rovalle'@'%' IDENTIFIED by 'N$@dmin.2020!';";
         //$sql = "UPDATE mysql.user SET Password=PASSWORD('$password') WHERE USER='$user' AND Host=”localhost”;";
 
-		if(mysqli_query($con, $sql1))
-		{
-			if(mysqli_query($con, $sql))
-			{		
-				$sql2 = "UPDATE users SET active = 1 where username = $user";	
-				if (mysqli_query($con, $sql2)) {
-					echo(json_encode('changed'));
-				} else{
-					http_response_code(423);
-					echo mysqli_error($con);
-					echo("<br>");
-					echo $sql;
-				}
-			}else{
-				http_response_code(422);
-				echo mysqli_error($con);
-				echo("<br>");
-				echo $sql;
-				
-			}
+		if(mysqli_query($con, $sql))
+		{		
+            $sql2 = "UPDATE users SET active = 1 where username = $user";	
+            if (mysqli_query($con, $sql2)) {
+                echo(json_encode('changed'));
+            } else{
+                http_response_code(423);
+                echo mysqli_error($con);
+                echo("<br>");
+                echo $sql;
+            }
+		}else{
+			http_response_code(422);
+            echo mysqli_error($con);
+            echo("<br>");
+            echo $sql;
+            
 		}
 	}
 ?>
