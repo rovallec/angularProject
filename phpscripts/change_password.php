5b2482a42b66d1360fd637dab3ed72286c14e404
avielman <avielman@gmail.com>
2021-07-19 09:34:09 -0600

    Se crea change_password.

---
+++ phpscripts/change_password.php
@@ -0,0 +1,29 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+	require 'database.php';
+	$postdata = file_get_contents("php://input");
+	if(isset($postdata) && !empty($postdata))
+	{
+		$request = json_decode($postdata);
+		
+        $user = ($request->username);
+		$password = ($request->password);
+		
+		//$sql = "ALTER USER ‘$user’@’localhost’ IDENTIFIED BY ‘$password’;";
+        $sql = "UPDATE mysql.user SET Password=PASSWORD(‘$password’) WHERE USER=’$user’ AND Host=”localhost”;"
+
+		if(mysqli_query($con, $sql))
+		{		
+            $sql2 = "UPDATE users SET
+            active = 1
+            where username = $user";	
+            if (mysqli_query($con, $sql2)) {
+                echo(json_encode('changed'));
+            }
+		}else{
+			http_response_code(422);
+            echo $sql;
+		}
+	}
+?>
