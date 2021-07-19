282935ea801cca27ef8aea3e5acbd1d1e06d86d3
rovallec <raul.ovalle@nearsol.gt>
2021-07-19 11:08:49 -0600

    a

--- phpscripts/change_password.php
+++ phpscripts/change_password.php
@@ -17,21 +17,14 @@ header('Access-Control-Allow-Headers: *');
 
 		if(mysqli_query($con, $sql))
 		{		
-            $sql2 = "UPDATE users SET active = 1 where username = $user";	
+            $sql2 = "UPDATE users SET active = 1 where username = '$user'";	
             if (mysqli_query($con, $sql2)) {
                 echo(json_encode('changed'));
             } else{
-                http_response_code(423);
                 echo mysqli_error($con);
-                echo("<br>");
-                echo $sql;
             }
-		}else{
-			http_response_code(422);
+		}else{			
             echo mysqli_error($con);
-            echo("<br>");
-            echo $sql;
-            
 		}
 	}
 ?>
