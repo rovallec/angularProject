81878eb028d30272a796413504ae1d1883d22fab
avielman <avielman@gmail.com>
2021-07-21 17:04:33 -0600

    Corrección menor

--- phpscripts/change_password.php
+++ phpscripts/change_password.php
@@ -10,20 +10,18 @@ header('Access-Control-Allow-Headers: *');
         $user = ($request->username);
 		$password = ($request->password);
 		
-		//$sql = "ALTER USER '$user'@’localhost’ IDENTIFIED BY ‘$password’;";
-        $sql = "SET password FOR 'rovalle'@'%' = 'N$@dmin.2020';";
-        //$sql = "UPDATE mysql.user SET Password=PASSWORD('$password') WHERE USER='$user' AND Host=”localhost”;";
+		$sql = "ALTER USER '$user'@'localhost' IDENTIFIED BY '$password';";
 
 		if(mysqli_query($con, $sql))
 		{		
-            $sql2 = "UPDATE users SET active = 1 where username = $user";	
+            $sql2 = "UPDATE users SET active = 1 where username = $user;";	
             if (mysqli_query($con, $sql2)) {
                 echo(json_encode('changed'));
             } else{
                 http_response_code(423);
                 echo mysqli_error($con);
                 echo("<br>");
-                echo $sql;
+                echo $sql2;
             }
 		}else{
 			http_response_code(422);
