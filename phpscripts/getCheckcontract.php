a0383ece44be57c75ea86fec354eac1f1669a6de
rovallec <raul.ovalle@nearsol.gt>
2021-07-23 11:18:03 -0600

    1

--- phpscripts/getCheckcontract.php
+++ phpscripts/getCheckcontract.php
@@ -66,6 +66,7 @@
     $yr = '';
 
     $sql = "SELECT `hires`.*, `employees`.*, `waves`.`ops_start`, `waves`.`base_payment` AS `base`, `waves`.`productivity_payment` AS `prod`, `profiles`.*, `education_details`.*, `contact_details`.* FROM `hires` LEFT JOIN `employees` ON `employees`.`id_hire` = `hires`.`idhires` LEFT JOIN `waves` ON `waves`.`idwaves` = `hires`.`id_wave` LEFT JOIN `profiles` ON `profiles`.`idprofiles` = `hires`.`id_profile` LEFT JOIN `education_details` ON `education_details`.`id_profile` = `profiles`.`idprofiles` LEFT JOIN `contact_details` ON `contact_details`.`id_profile` = `profiles`.`idprofiles` WHERE `idemployees` = '$id';";
+
     if($result = mysqli_query($con, $sql)){
         while($row = mysqli_fetch_assoc($result)){
             $name = $row['first_name'] . " " . $row['second_name'] . " " . $row['first_lastname'] . " " . $row['second_lastname'];
@@ -78,8 +79,6 @@
             $gender = '';
             $gender = $row['gender'];
             $gender = $gender . ", " .$row['marital_status'];
-
-            if($platform == 'ON SITE' || $platform == 'WAH'){
 
             $gender = $gender . ", " . $row['profesion'] . ", " . $row['nationality'];
 
@@ -136,50 +135,48 @@
             $mn = $mnth[intval($dt_end[1])-1];
             $yr = $dt_end[0];
         }
-    }
-    $res['name'] = $name;
-    $res['birthday'] = $birthday;
-    $res['year'] = $year;
-    $res['today'] = $today;
-    $res['now'] = $now;
-    $res['age'] = $age;
-    $res['platform'] = $platform;
-    $res['gender'] = $gender;
-    $res['address'] = $address;
-    $res['municipio'] = $municipio;
-    $res['dpi_n'] = $dpi_n;
-    $res['hiring_date'] = $hiring_date;
-    $res['job'] = $job;
-    $res['base_n_n'] = $base_n_n;
-    $res['incentivo_n_n'] = $incentivo_n_n;
-    $res['total_n_n'] = $total_n_n;
-    $res['base_n'] = $base_n;
-    $res['incentivo_exp'] = $incentivo_exp;
-    $res['incentivo_n'] = $incentivo_n;
-    $res['total_n'] = $total_n;
-    $res['dpi_1'] = $dpi_1;
-    $res['dpi_2'] = $dpi_2;
-    $res['dpi_3'] = $dpi_3;
-    $res['dpi_4'] = $dpi_4;
-    $res['t'] = $t;
-    $res['base_n_init'] = $base_n_init;
-    $res['base_n_int_l'] = $base_n_int_l;
-    $res['base_n_cent_l'] = $base_n_cent_l;
-    $res['incentivo_n_init'] = $incentivo_n_init;
-    $res['incentivo_n_int_l'] = $incentivo_n_int_l;
-    $res['incentivo_n_cent_l'] = $incentivo_n_cent_l;
-    $res['total_n_init'] = $total_n_init;
-    $res['total_n_int_l'] = $total_n_int_l;
-    $res['total_n_cent_l'] = $total_n_cent_l;
-    $res['dt'] = $dt;
-    $res['date_letters'] = $date_letters;
-    $res['today_date'] = $today_date;
-    $res['dt_end'] = $dt_end;
-    $res['day'] = $day;
-    $res['mn'] = $mn;
-    $res['yr'] = $yr;
-    echo("test");
-    echo(json_encode($res));
+        $res['name'] = $name;
+        $res['birthday'] = $birthday;
+        $res['year'] = $year;
+        $res['today'] = $today;
+        $res['now'] = $now;
+        $res['age'] = $age;
+        $res['platform'] = $platform;
+        $res['gender'] = $gender;
+        $res['address'] = $address;
+        $res['municipio'] = $municipio;
+        $res['dpi_n'] = $dpi_n;
+        $res['hiring_date'] = $hiring_date;
+        $res['job'] = $job;
+        $res['base_n_n'] = $base_n_n;
+        $res['incentivo_n_n'] = $incentivo_n_n;
+        $res['total_n_n'] = $total_n_n;
+        $res['base_n'] = $base_n;
+        $res['incentivo_exp'] = $incentivo_exp;
+        $res['incentivo_n'] = $incentivo_n;
+        $res['total_n'] = $total_n;
+        $res['dpi_1'] = $dpi_1;
+        $res['dpi_2'] = $dpi_2;
+        $res['dpi_3'] = $dpi_3;
+        $res['dpi_4'] = $dpi_4;
+        $res['t'] = $t;
+        $res['base_n_init'] = $base_n_init;
+        $res['base_n_int_l'] = $base_n_int_l;
+        $res['base_n_cent_l'] = $base_n_cent_l;
+        $res['incentivo_n_init'] = $incentivo_n_init;
+        $res['incentivo_n_int_l'] = $incentivo_n_int_l;
+        $res['incentivo_n_cent_l'] = $incentivo_n_cent_l;
+        $res['total_n_init'] = $total_n_init;
+        $res['total_n_int_l'] = $total_n_int_l;
+        $res['total_n_cent_l'] = $total_n_cent_l;
+        $res['dt'] = $dt;
+        $res['date_letters'] = $date_letters;
+        $res['today_date'] = $today_date;
+        $res['dt_end'] = $dt_end;
+        $res['day'] = $day;
+        $res['mn'] = $mn;
+        $res['yr'] = $yr;
+        echo(json_encode($res));
 }
 ?>
 
