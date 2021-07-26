c41b5fa0a304a7824e0cb4e91dbc57270f8f93ee
rovallec <raul.ovalle@nearsol.gt>
2021-07-26 06:24:36 -0600

    a

--- phpscripts/update_profile.php
+++ phpscripts/update_profile.php
@@ -40,7 +40,7 @@ header('Access-Control-Allow-Headers: *');
 			$sql_1 = "SELECT * FROM `contact_details` WHERE id_profile = $idprofiles LIMIT 1";
 
 			if($result = mysqli_query($con, $sql_1)){
-				if(($result) > 0){
+				if(mysqli_num_rows($result) > 0){
 					$sql2 = "UPDATE `contact_details` set `primary_phone` = '{$primary_phone}', `secondary_phone` = '{$secondary_phone}', `address` = '{$address}', `city` = '{$city}', `email` = '{$email}' WHERE `id_profile` = '{$idprofiles}';";
 				}else{
 					$sql2 = "INSERT INTO `contact_details` (`idcontact_details`, `id_profile`, `primary_phone`, `secondary_phone`, `address`, `city`, `email`) VALUES (NULL, $idprofiles, '$primary_phone', '$secondary_phone', '$address', '$city', '$email');";
