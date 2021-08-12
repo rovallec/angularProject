9696be9e8e195612d616e7e09b4035805ab1d0eb
rovallec <raul.ovalle@nearsol.gt>
2021-08-12 13:01:08 -0600

    test

--- phpscripts/updateCredits.php
+++ phpscripts/updateCredits.php
@@ -11,12 +11,19 @@ $status = ($request->status);
 
 if($status == 'EDIT'){
     $sql = "UPDATE `credits` SET `type` = $type, `amount` = $amount;";
-    $sql2 = "INSERT INTO `internal_processes` (`idinternal_processes`, `id_user`, `id_employee`, `name`, `date`, `status`, `notes`) VALUES (NULL, $id_user, $id_employee, 'Update credit', DATE_FORMAT(NOW(), '%Y-%m-%d'), 'COMPLETED', CONCAT('Manualy Deleted AT ', NOW()));"
+    $sql2 = "INSERT INTO `internal_processes` (`idinternal_processes`, `id_user`, `id_employee`, `name`, `date`, `status`, `notes`) VALUES 
+    (NULL, $id_user, $id_employee, 'Update credit', DATE_FORMAT(NOW(), '%Y-%m-%d'), 'COMPLETED', CONCAT('Manualy Deleted AT ', NOW(), ' | ', 
+    (SELECT CONCAT_WS(',',COALESCE(idcredits, 'NULL'), COALESCE(id_payment, 'NULL'),
+    COALESCE(type, 'NULL'), COALESCE(amount, 'NULL'), COALESCE(status, 'NULL')) AS `cred` FROM credits WHERE idcredits = $idcredits)));";
+    if(mysqli_query($con,$sql2)){
+        if(mysqli_query($con,$sql)){
+            echo(mysqli_insert_id($con));
+        }
+    }
 }else{
     $sql = "UPDATE `credits` SET `status` = '$status' WHERE `idcredits` = $idcredits";
-}
-
-if(mysqli_query($con,$sql)){
-    echo(mysqli_insert_id($con));
+    if(mysqli_query($con,$sql)){
+        echo(mysqli_insert_id($con));
+    }
 }
 ?>
