c7e7f0e100e994dcc806800b7ed8c098a36d3201
rovallec <raul.ovalle@nearsol.gt>
2021-08-12 14:10:55 -0600

    test

--- phpscripts/updateCredits.php
+++ phpscripts/updateCredits.php
@@ -16,7 +16,7 @@ $id_employee = ($request->id_employee);
 if($status == 'EDIT'){
     $sql = "UPDATE `credits` SET `type` = '$type', `amount` = '$amount' WHERE `idcredits` = $idcredits;";
     $sql2 = "INSERT INTO `internal_processes` (`idinternal_processes`, `id_user`, `id_employee`, `name`, `date`, `status`, `notes`) VALUES 
-    (NULL, $id_user, $id_employee, 'Update credit', DATE_FORMAT(NOW(), '%Y-%m-%d'), 'COMPLETED', CONCAT('Manualy Deleted AT ', NOW(), ' | ', 
+    (NULL, $id_user, $id_employee, 'Update credit', DATE_FORMAT(NOW(), '%Y-%m-%d'), 'COMPLETED', CONCAT('Manualy EDITED AT ', NOW(), ' | ', 
     (SELECT CONCAT_WS(',',COALESCE(idcredits, 'NULL'), COALESCE(id_payment, 'NULL'),
     COALESCE(type, 'NULL'), COALESCE(amount, 'NULL'), COALESCE(status, 'NULL')) AS `cred` FROM credits WHERE idcredits = $idcredits)));";
     if(mysqli_query($con,$sql2)){
