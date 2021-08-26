b1cd324d31c61ac3db22b91abd8577dc65e48777
rovallec <raul.ovalle@nearsol.gt>
2021-08-26 15:01:17 -0600

    a

--- phpscripts/getRosterTemplates.php
+++ phpscripts/getRosterTemplates.php
@@ -10,18 +10,18 @@ $proccesses = [];
 $i = 0;
 
 
-if(strpos($str, "=") === FALSE){
+if(strpos($str, "=")){
     $sql = "SELECT roster_types.*, a.start AS `mon_start`, a.end AS `mon_end`, b.start AS `tue_start`, b.end AS `tue_end`, c.start AS `wed_start`, c.end AS `wed_end`,
-               d.start AS `thur_start`, d.end AS `thur_end`, e.start AS `fri_start`, e.end AS `fri_end`, f.start AS `sat_start`, f.end AS `sat_end`,
-               g.start AS `sun_start`, g.end AS `sun_end` FROM roster_types
-        INNER JOIN roster_times a ON a.idroster_times = roster_types.id_time_mon
-        INNER JOIN roster_times b ON b.idroster_times = roster_types.id_time_tue
-        INNER JOIN roster_times c ON c.idroster_times = roster_types.id_time_wed
-        INNER JOIN roster_times d ON d.idroster_times = roster_types.id_time_thur
-        INNER JOIN roster_times e ON e.idroster_times = roster_types.id_time_fri
-        INNER JOIN roster_times f ON f.idroster_times = roster_types.id_time_sat
-        INNER JOIN roster_times g ON g.idroster_times = roster_types.id_time_sun
-        WHERE $str;";
+    d.start AS `thur_start`, d.end AS `thur_end`, e.start AS `fri_start`, e.end AS `fri_end`, f.start AS `sat_start`, f.end AS `sat_end`,
+    g.start AS `sun_start`, g.end AS `sun_end` FROM roster_types
+    INNER JOIN roster_times a ON a.idroster_times = roster_types.id_time_mon
+    INNER JOIN roster_times b ON b.idroster_times = roster_types.id_time_tue
+    INNER JOIN roster_times c ON c.idroster_times = roster_types.id_time_wed
+    INNER JOIN roster_times d ON d.idroster_times = roster_types.id_time_thur
+    INNER JOIN roster_times e ON e.idroster_times = roster_types.id_time_fri
+    INNER JOIN roster_times f ON f.idroster_times = roster_types.id_time_sat
+    INNER JOIN roster_times g ON g.idroster_times = roster_types.id_time_sun
+    WHERE tag = '$str';"
 }else{
     $sql = "SELECT roster_types.*, a.start AS `mon_start`, a.end AS `mon_end`, b.start AS `tue_start`, b.end AS `tue_end`, c.start AS `wed_start`, c.end AS `wed_end`,
                d.start AS `thur_start`, d.end AS `thur_end`, e.start AS `fri_start`, e.end AS `fri_end`, f.start AS `sat_start`, f.end AS `sat_end`,
@@ -33,7 +33,7 @@ if(strpos($str, "=") === FALSE){
         INNER JOIN roster_times e ON e.idroster_times = roster_types.id_time_fri
         INNER JOIN roster_times f ON f.idroster_times = roster_types.id_time_sat
         INNER JOIN roster_times g ON g.idroster_times = roster_types.id_time_sun
-        WHERE tag = '$str';";
+        WHERE $str;";
 }
 
 
