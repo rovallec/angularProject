63e7e3792ab20f366ac6e6a89e6fdcc5b08cf2a1
rovallec <raul.ovalle@nearsol.gt>
2021-08-27 11:07:37 -0600

    test

--- phpscripts/updateRoosterType.php
+++ phpscripts/updateRoosterType.php
@@ -3,6 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 require 'database.php';
 $proccesses = [];
+$i = 0;
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
