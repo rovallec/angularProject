50e7cdff09008ab14c8147f9b8231df0dd2d7662
rovallec <raul.ovalle@nearsol.gt>
2021-09-10 14:44:21 -0600

    test

--- phpscripts/insertCancelAdjustment.php
+++ phpscripts/insertCancelAdjustment.php
@@ -10,5 +10,11 @@ $notes = ($request->notes);
 $id_process = ($request->id_process);
 $date = date('Y-m-d');
 $sql = "UPDATE hr_processes SET `notes` = CONCAT(notes, ' | REVERTED BY $notes AT ', now()) WHERE `idhr_processes` = $id_process;";
-echo($sql);
+
+if(mysqli_query($con,$sql)){
+	http_response_code(200);
+}else{
+  	http_response_code(400);
+	echo($sql);
+}
 ?>
