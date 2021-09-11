e1fc622fc272a9512ab103a7c0da4d5d45326eb7
rovallec <raul.ovalle@nearsol.gt>
2021-09-10 20:03:18 -0600

    test

--- phpscripts/updateRosterOrder.php
+++ phpscripts/updateRosterOrder.php
@@ -18,13 +18,13 @@ if($result = $transact->query($sql)){
     $row = $result->fetch_assoc();
     $id_new_1 = ((int)$row['idrosters']) + 1;
     $id_new_2 = ((int)$row['idrosters']) + 2;
-    $sql2 = "UPDATE rosters SET idrosters = $id_new_1 WHERE idrosters = $id_old_1";
-    $sql3 = "UPDATE rosters SET idrosters = $id_new_2 WHERE idrosters = $id_old_2";
+    $sql2 = "UPDATE rosters SET idrosters = $id_new_1 WHERE idrosters = $id_old_1;";
+    $sql3 = "UPDATE rosters SET idrosters = $id_new_2 WHERE idrosters = $id_old_2;";
     if($result = $transact->query($sql2)){
         if($result = $transact->query($sql3)){
-            $sql4 = "UPDATE rosters SET idrosters = $id_old_2 WHERE idrosters = $id_new_1";
-            $sql5 = "UPDATE rosters SET idrosters = $id_old_1 WHERE idrosters = $id_new_2";
-            echo($sql . $sql2 . $sql3 . $sql4 . $sql4);
+            $sql4 = "UPDATE rosters SET idrosters = $id_old_2 WHERE idrosters = $id_new_1;";
+            $sql5 = "UPDATE rosters SET idrosters = $id_old_1 WHERE idrosters = $id_new_2;";
+            echo($sql . $sql2 . $sql3 . $sql4 . $sql5);
             if($result = $transact->query($sql4)){
                 if($result = $transact->query($sql5)){
                     $transact->commit();
