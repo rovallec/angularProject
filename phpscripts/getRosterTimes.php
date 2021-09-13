43d0c5b0986241c6ccfc39b5e862acd8128c9edf
rovallec <raul.ovalle@nearsol.gt>
2021-09-13 17:03:44 -0600

    test

--- phpscripts/getRosterTimes.php
+++ phpscripts/getRosterTimes.php
@@ -12,7 +12,7 @@ if($result = mysqli_query($con, $sql)){
         $user[$i]['idroster_times'] = $row['idroster_times'];
         $user[$i]['start'] = $row['start'];
         $user[$i]['end'] = $row['end'];
-        $user[$i]['fixed'] = $row['fixed'];
+        $user[$i]['fixed'] = $row['fixed_schedule'];
         $i++;
     };
     echo json_encode($user);
