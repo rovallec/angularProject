d0db09006d2b8bbe02771dc4a27eca99a1805a3a
avielman <avielman@gmail.com>
2021-10-22 08:00:40 -0600

    Se implementa el tipo StringOrNumber, para evitar errores en el editor.

--- src/app/process_templates.ts
+++ src/app/process_templates.ts
@@ -2,6 +2,8 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { employees } from './fullProcess';
 import { profiles, profiles_family, profiles_histories } from './profiles';
 
+
+export type StringOrNumber = number | string;
 export class process_templates {
     idprocess_templates: string;
     name: string;
@@ -169,7 +171,7 @@ export class attendences {
     day_off2: string;
     status: string;
     id_wave: string;
-    balance: string;
+    balance: StringOrNumber;
     state: string;
     igss:string;
     tk_exp:string;
