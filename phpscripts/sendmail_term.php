492ab103e0dd9347bb0a809e8b5ae89c0bff6f0f
avielman <avielman@gmail.com>
2021-11-01 13:32:28 -0600

    Corrección menor al query para evitar ambiguedad.

--- phpscripts/sendmail_term.php
+++ phpscripts/sendmail_term.php
@@ -42,13 +42,13 @@ if($result = mysqli_query($con, $sql))
 			INNER JOIN accounts ON accounts.idaccounts = employees.id_account
 			LEFT JOIN (SELECT SUM(vacations.count) AS `count`, hr_processes.id_employee 
 						FROM vacations 
-						INNER JOIN hr_processes ON hr_processes.idhr_processes = vacations.id_process WHERE id_type = 4 AND status != 'DISMISSED'
+						INNER JOIN hr_processes ON hr_processes.idhr_processes = vacations.id_process WHERE hr_processes.id_type = 4 AND hr_processes.status != 'DISMISSED'
 						GROUP BY id_employee) AS `vac` ON `vac`.id_employee = employees.idemployees
-			LEFT JOIN (SELECT 'SI' AS `insurance`, id_employee FROM insurances INNER JOIN hr_processes ON hr_processes.idhr_processes = insurances.id_process) 
+			LEFT JOIN (SELECT 'SI' AS `insurance`, hr_processes.id_employee FROM insurances INNER JOIN hr_processes ON hr_processes.idhr_processes = insurances.id_process) 
 						AS `insurance` ON `insurance`.id_employee = employees.idemployees
 			INNER JOIN users ON users.idUser = employees.reporter
 			LEFT JOIN (SELECT users.user_name, processes.id_profile FROM processes INNER JOIN users ON users.idUser = processes.id_user WHERE name = 'First Interview')
-					   	AS `first_interview` ON `first_interview`.id_profile = profiles.idprofiles WHERE employees.idemployees = $id_employee ORDER BY valid_from ASC LIMIT 1";
+					   	AS `first_interview` ON `first_interview`.id_profile = profiles.idprofiles WHERE employees.idemployees = $id_employee ORDER BY valid_from ASC LIMIT 1;";
 	if($result = mysqli_query($con, $sql2)){
 		while($row = mysqli_fetch_assoc($result))
 		{
