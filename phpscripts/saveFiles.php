9156f5c5e446185f9cf8b9f68cc494a334b227f6
avielman <avielman@gmail.com>
2021-11-05 07:30:05 -0600

    Corrección menor.

--- phpscripts/saveFiles.php
+++ phpscripts/saveFiles.php
@@ -12,7 +12,7 @@ $name = validarDatos($request->name);
 $tipo = validarDatos($request->type);
 $id = validarDatos($request->id);
 
-$sql = "select idfiles from file where id_process = $id;";
+$sql = "SELECT idfiles FROM file WHERE id_process = $id;";
 
 if($res = mysqli_query($con, $sql)){
   $r = mysqli_fetch_assoc($res);
@@ -31,15 +31,15 @@ if($res = mysqli_query($con, $sql)){
       http_response_code(421);
     }    
   } else {
-$sql1 = "INSERT INTO file (idfiles, id_process, name, tipo, archivo) VALUES (NULL, $id, '$name', '$tipo', '$archivo_binario');";  
+    $sql2 = "INSERT INTO file (idfiles, id_process, name, tipo, archivo) VALUES (NULL, $id, '$name', '$tipo', '$archivo_binario');";  
 
-    if(mysqli_query($con, $sql1)){
+    if(mysqli_query($con, $sql2)){
       $idfile = mysqli_insert_id($con);
       echo(json_encode("Uplodated id: " . $idfile));
       //echo(json_encode('Insertar');
     } else {
-      $error = "Query: " . $sql1;
-      //$error = "0|" . mysqli_error($con) . ' query: ' . $sql1;
+      $error = "Query: " . $sql2;
+      //$error = "0|" . mysqli_error($con) . ' query: ' . $sql2;
       echo(json_encode($error));
       http_response_code(422);
     }    
