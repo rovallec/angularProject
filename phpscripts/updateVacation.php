2e9e33a963d52173d34b2d5d97052bfa4e2d7221
avielman <avielman@gmail.com>
2021-12-10 09:28:06 -0600

    Corrección menor al update vacations.

--- phpscripts/updateVacation.php
+++ phpscripts/updateVacation.php
@@ -7,9 +7,10 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 $id_process = ($request->id_process);
-$status = ($request->status)
+$status = ($request->status);
+$id_user = ($request->id_user);
 
-$sql = "UPDATE `hr_processes` SET `status` = '$status', `notes` = `notes` . '|| ' . $status . ' By ' WHERE `idhr_processes` = $id_process;";
+$sql = "UPDATE hr_processes SET status = '$status', notes = CONCAT(notes, '" . '|| ' .$status.' By '. $id_user. "') WHERE idhr_processes = $id_process;";
 
 if(mysqli_query($con, $sql)){
     echo(json_encode('1'));
@@ -18,4 +19,3 @@ if(mysqli_query($con, $sql)){
     echo(json_encode($sql));
     http_response_code(404);
 }
-?>
