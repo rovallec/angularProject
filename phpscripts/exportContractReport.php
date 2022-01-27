7ebadbd00f71c92a900526f6fe23b657ae65b78b
rovallec <raul.ovalle@nearsol.gt>
2022-01-27 15:09:14 -0600

    test

--- phpscripts/exportContractReport.php
+++ phpscripts/exportContractReport.php
@@ -10,47 +10,47 @@ echo "\xEF\xBB\xBF";
 $i = 0;
 
 $sql = "SELECT 
-          @i := @i + 1 AS 'No.',
-          a.name AS account,
-          w.name as wave,
-          p2.name, 
-          cd.address,
-          cd.city AS neighborhood,
-          ed.current_level AS title,
-          p.gender,
-          p.nationality,
-          p.dpi,
-          p.day_of_birth AS birthday,
-          (select TIMESTAMPDIFF(year, p.day_of_birth, now())) AS age,
-          p.marital_status,
-          e.hiring_date,
-          DAY(e.hiring_date) AS hiring_day,
-          MONTH(e.hiring_date) AS hiring_month,
-          YEAR(e.hiring_date) AS hiring_year,
-          'HR' AS revised,
-          0 as number_contract,
-          YEAR('contract_date') AS contract_year,
-          'Salario base en letras' AS salary_letter,
-          e.base_payment,
-          'Bonificación en letras' AS bonus_letter,
-          (e.productivity_payment - 250.00) AS productivity_payment,
-          250.00 AS '78-89',
-          'dos cientos cincuenta quetzales exactos' as '78-89_letter',
-          e.job,
-          'funciones varias' as 'functions',
-          'Salario total en letras' AS total_letter,
-          e.base_payment + e.productivity_payment AS total_salary
-        FROM employees e
-        INNER JOIN hires h ON h.idhires = e.id_hire 
-        INNER JOIN (SELECT UPPER(CONCAT(TRIM(p1.first_name), ' ', TRIM(p1.second_name), ' ', TRIM(p1.first_lastname), ' ', TRIM(p1.second_lastname))) AS name, p1.idprofiles FROM profiles p1) p2 ON (p2.idprofiles = h.id_profile)
-        INNER JOIN profiles p ON (h.id_profile = p.idprofiles)
-        INNER JOIN accounts a ON a.idaccounts = e.id_account
-        INNER JOIN users u ON u.idUser = e.reporter
-        INNER JOIN waves w ON (h.id_wave = w.idwaves)
-        LEFT JOIN (select * from profiles p2 INNER join contact_details cd1 on (p2.idprofiles = cd1.id_profile) order by cd1.idcontact_details desc limit 1) cd ON (p.idprofiles = cd.id_profile)
-        LEFT JOIN education_details ed ON (p.idprofiles = ed.ideducation_details)
-        CROSS JOIN (SELECT @i := 0) r
-        ORDER BY a.name ASC ;";
+@i := @i + 1 AS 'No.',
+a.name AS account,
+w.name as wave,
+p2.name, 
+cd.address,
+cd.city AS neighborhood,
+ed.current_level AS title,
+p.gender,
+p.nationality,
+p.dpi,
+p.day_of_birth AS birthday,
+(select TIMESTAMPDIFF(year, p.day_of_birth, now())) AS age,
+p.marital_status,
+e.hiring_date,
+DAY(e.hiring_date) AS hiring_day,
+MONTH(e.hiring_date) AS hiring_month,
+YEAR(e.hiring_date) AS hiring_year,
+'HR' AS revised,
+0 as number_contract,
+YEAR('contract_date') AS contract_year,
+'Salario base en letras' AS salary_letter,
+e.base_payment,
+'Bonificación en letras' AS bonus_letter,
+(e.productivity_payment - 250.00) AS productivity_payment,
+250.00 AS '78-89',
+'dos cientos cincuenta quetzales exactos' as '78-89_letter',
+e.job,
+'funciones varias' as 'functions',
+'Salario total en letras' AS total_letter,
+e.base_payment + e.productivity_payment AS total_salary
+FROM employees e
+INNER JOIN hires h ON h.idhires = e.id_hire 
+INNER JOIN (SELECT UPPER(CONCAT(TRIM(p1.first_name), ' ', TRIM(p1.second_name), ' ', TRIM(p1.first_lastname), ' ', TRIM(p1.second_lastname))) AS name, p1.idprofiles FROM profiles p1) p2 ON (p2.idprofiles = h.id_profile)
+INNER JOIN profiles p ON (h.id_profile = p.idprofiles)
+INNER JOIN accounts a ON a.idaccounts = e.id_account
+INNER JOIN users u ON u.idUser = e.reporter
+INNER JOIN waves w ON (h.id_wave = w.idwaves)
+LEFT JOIN contact_details AS cd ON (p.idprofiles = cd.id_profile)
+LEFT JOIN education_details ed ON (p.idprofiles = ed.ideducation_details)
+CROSS JOIN (SELECT @i := 0) r
+ORDER BY a.name ASC ;"
 
 
 $title = ['NO.', 'Account', 'Wave', 'Nombre Completo', 'Dirección', 'Vecindad', 'Título', 'Genero', 'nacionalidad', 'DPI', 'Letras', 'Fecha de Nacimineto', 'Edad #', 'Edad (Letras)', 'Estado Civil', 'Fecha Ingreso', 'dia', 'mes', 'año', 'Revisados', 'NUMERO DE CONTRATO', 'CONTRACT YEAR', 'SALARIO BASE', 'SALARIO BASE #', 'BONIFICACION INCENTIVO', '78-89', 'Bono eficiencia #', 'bono ef letras', 'puesto', 'funciones', 'Salario Total', 'Salario Tot #'];
@@ -65,14 +65,42 @@ if($result = mysqli_query($con,$sql)){
     $exportRow[4] = $row['address'];
     $exportRow[5] = $row['neighborhood'];
     $exportRow[6] = $row['title'];
-    $exportRow[7] = $row['gender'];
+    switch ($row['gender']) {
+      case 'FEMALE':
+        $gender = 'Femenino';
+      case 'MALE':
+        $gender = 'Masculino';
+        break;
+    }
+    $exportRow[7] = $gender;
     $exportRow[8] = $row['nationality'];
     $exportRow[9] = $row['dpi'];
     $exportRow[10] = dpi_letter($row['dpi']);
     $exportRow[11] = $row['birthday'];
     $exportRow[12] = $row['age'];
     $exportRow[13] = number_letter($row['age']);
-    $exportRow[14] = $row['marital_status'];
+    switch ($row['marital_status']) {
+      case 'SINGLE':
+        if ($marrital_status == 'Femenino') {
+          $marrital_status = $marrital_status . ", " . 'Soltera';
+        } else if ($marrital_status == 'Masculino') {
+          $marrital_status = $marrital_status . ", " . 'Soltero';
+        }
+      case 'MARRIED':
+        if ($marrital_status == 'Femenino') {
+          $marrital_status = $marrital_status . ", " . 'Casada';
+        } else if ($marrital_status == 'Masculino') {
+          $marrital_status = $marrital_status . ", " . 'Cadado';
+        }
+      case 'DIVORCEE':
+        if ($marrital_status == 'Femenino') {
+          $marrital_status = $marrital_status . ", " . 'Divorciada';
+        } else if ($marrital_status == 'Masculino') {
+          $marrital_status = $marrital_status . ", " . 'Divorciado';
+        }
+      break;
+    }
+    $exportRow[14] = $marrital_status;
     $exportRow[15] = $row['hiring_date'];
     $exportRow[16] = $row['hiring_day'];
     $exportRow[17] = $row['hiring_month'];
