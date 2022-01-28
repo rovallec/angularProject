59050581e92cc307c0d59f1bc596ec04327cc8e7
rovallec <raul.ovalle@nearsol.gt>
2022-01-28 11:03:05 -0600

    asdf

--- phpscripts/irtraNewcarnet.php
+++ phpscripts/irtraNewcarnet.php
@@ -29,7 +29,7 @@ $second_name = $_GET['second_name'];
 $zone = $_GET['zone'];
 $email = $_GET['email'];
 $spouse_name = $_GET['spouse_name'];
-$spouse_lastname = $_GET['spouse_lastname'];
+$spouse_lastname = str_replace(',', ' ', $_GET['spouse_lastname']);
 $extended_dept = $_GET['depto'];
 $extended_mun = $_GET['mun'];
 
