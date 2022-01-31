bb6f1998de1bac4a790d6a4a7eb5216c067f88f0
rovallec <raul.ovalle@nearsol.gt>
2022-01-31 14:50:25 -0600

    sessions

--- phpscripts/getSessions.php
+++ phpscripts/getSessions.php
@@ -14,13 +14,13 @@ $sql = "SELECT * FROM users WHERE idUser = $id";
 if($result = mysqli_query($con, $sql)){
     if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)){
-            $authUser[$i]['iduser'] = $row['idUser'];
-			$authUser[$i]['username'] = $row['username'];
-			$authUser[$i]['department'] = $row['department'];
-			$authUser[$i]['user_name'] = $row['user_name'];
-			$authUser[$i]['valid'] = $row['valid'];
-			$authUser[$i]['id_role'] = $row['id_role'];
-			$authUser[$i]['signature'] = $row['signature'];
+            $authUser['iduser'] = $row['idUser'];
+			$authUser['username'] = $row['username'];
+			$authUser['department'] = $row['department'];
+			$authUser['user_name'] = $row['user_name'];
+			$authUser['valid'] = $row['valid'];
+			$authUser['id_role'] = $row['id_role'];
+			$authUser['signature'] = $row['signature'];
         }
         echo(json_encode($term));
     }else{
