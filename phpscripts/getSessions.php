821334b4de5792bd99d7a49a1386db37e41cc538
rovallec <raul.ovalle@nearsol.gt>
2022-01-31 14:41:47 -0600

    sessions

---
+++ phpscripts/getSessions.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+require 'database.php';
+
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+$id = ($request->iduser);
+
+$term = [];
+$rss = '';
+$sql = "SELECT * FROM users WHERE idUser = $id";
+if($result = mysqli_query($con, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            $authUser[$i]['iduser'] = $row['idUser'];
+			$authUser[$i]['username'] = $row['username'];
+			$authUser[$i]['department'] = $row['department'];
+			$authUser[$i]['user_name'] = $row['user_name'];
+			$authUser[$i]['valid'] = $row['valid'];
+			$authUser[$i]['id_role'] = $row['id_role'];
+			$authUser[$i]['signature'] = $row['signature'];
+        }
+        echo(json_encode($term));
+    }else{
+        $rss = '0';
+        echo(json_encode($rss));
+    }
+}
+?>
