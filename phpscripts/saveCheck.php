8e45157064a4631d7b5fcfb9e6dbb866c21cea81
avielman <avielman@gmail.com>
2022-02-02 00:43:22 -0600

    .

--- phpscripts/saveCheck.php
+++ phpscripts/saveCheck.php
@@ -94,7 +94,7 @@ try
           http_response_code(423);
         } // end if SQL 3
       } // End foreach
-      echo(json_encode("Success | " . $id_check . " | Proceso ejecutado correctamente."));
+      //echo(json_encode("Success | " . $id_check . " | Proceso ejecutado correctamente."));
       http_response_code(200);
     } else {
       $error = mysqli_error($transact);
