f1c68de106837320ba8f135186ac7649f661a4b7
rovallec <raul.ovalle@nearsol.gt>
2022-03-24 21:06:15 -0600

    ID Null

--- phpscripts/insertProvider.php
+++ phpscripts/insertProvider.php
@@ -20,7 +20,7 @@ $contract_start = ($request->contract_start);
 $contract_end = ($request->contract_end);
 $no_iva = ($request->no_iva);
 
-$sql = "INSERT INTO providers VALUES ($name, $contact, $phone, $cel, $email, $service, $credit, $conditions, $days, $contract_start, $contract_end, $no_iva)";
+$sql = "INSERT INTO providers VALUES (NULL, $name, $contact, $phone, $cel, $email, $service, $credit, $conditions, $days, $contract_start, $contract_end, $no_iva)";
 
 if(mysqli_query($con, $sql)){
     echo(json_encode('1'));
